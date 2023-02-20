66877dd12f9b3485ace73e74d4764b548b3d95f5
Sasha Osin <sashaosin364@gmail.com>
2023-02-20 18:27:45 +0200

    add tempates

--- store/products/views.py
+++ store/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {'title':'test Title','username':'Sasha'}
+    return render(request,'products/index.html',context=context)
+
+
+def products(request):
+    context = {'title':'Каталог'}
+    return render(request,'products/products.html')
