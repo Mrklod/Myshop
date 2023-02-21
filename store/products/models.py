40cf704b6beee47666b215f301a7c68fddf0e0b1
Sasha Osin <sashaosin364@gmail.com>
2023-02-21 13:47:07 +0200

    add models

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128,unique=True)
+    description = models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(Category,on_delete=models.PROTECT)
